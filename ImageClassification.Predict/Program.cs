b7826bd36488e82612ea909826fcb9921a7c9556
Katarzyna Czerwinska <s17098@pjwstk.edu.pl>
2021-01-20 20:13:44 +0100

    #19 Extracted methods in ImageClassification.Predict

--- ImageClassification.Predict/Program.cs
+++ ImageClassification.Predict/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using ImageClassification.DataModels;
@@ -23,69 +24,15 @@ namespace ImageClassification.Predict
                 var mlContext = new MLContext(seed: 1);
 
                 Console.WriteLine($"Loading model from: {imageClassifierModelZipFilePath}");
-
-                // Load the model
-                var loadedModel = mlContext.Model.Load(imageClassifierModelZipFilePath, out var modelInputSchema);
-
-                // Create prediction engine to try a single prediction (input = ImageData, output = ImagePrediction)
-                var predictionEngine = mlContext.Model.CreatePredictionEngine<InMemoryImageData, ImagePrediction>(loadedModel);
-
-                //Predict the first image in the folder
-                var imagesToPredict = FileUtils.LoadInMemoryImagesFromDirectory(imagesFolderPathForPredictions, false);
-
+                ITransformer loadedModel = GetLoadedModel(imageClassifierModelZipFilePath, mlContext);
+                var predictionEngine = GetPredictionEngine(mlContext, loadedModel);
+                var imagesToPredict = GetImagesToPredict(imagesFolderPathForPredictions);
                 var imageToPredict = imagesToPredict.First();
-
-                // Measure #1 prediction execution time.
-                var watch = System.Diagnostics.Stopwatch.StartNew();
-
-                var prediction = predictionEngine.Predict(imageToPredict);
-
-                // Stop measuring time.
-                watch.Stop();
-                var elapsedMs = watch.ElapsedMilliseconds;
-                Console.WriteLine("First Prediction took: " + elapsedMs + "mlSecs");
-
-                // Measure #2 prediction execution time.
-                var watch2 = System.Diagnostics.Stopwatch.StartNew();
-
-                var prediction2 = predictionEngine.Predict(imageToPredict);
-
-                // Stop measuring time.
-                watch2.Stop();
-                var elapsedMs2 = watch2.ElapsedMilliseconds;
-                Console.WriteLine("Second Prediction took: " + elapsedMs2 + "mlSecs");
-
-                // Get the highest score and its index
-                var maxScore = prediction.Score.Max();
-
-                ////////
-                // Double-check using the index
-                var maxIndex = prediction.Score.ToList().IndexOf(maxScore);
-                VBuffer<ReadOnlyMemory<char>> keys = default;
-                predictionEngine.OutputSchema[3].GetKeyValues(ref keys);
-                var keysArray = keys.DenseValues().ToArray();
-                var predictedLabelString = keysArray[maxIndex];
-                ////////
-
-                Console.WriteLine($"Image Filename : [{imageToPredict.ImageFileName}], " +
-                                  $"Predicted Label : [{prediction.PredictedLabel}], " +
-                                  $"Probability : [{maxScore}] "
-                                  );
-
-                //Predict all images in the folder
-                //
-                Console.WriteLine("");
-                Console.WriteLine("Predicting several images...");
-
-                foreach (var currentImageToPredict in imagesToPredict)
-                {
-                    var currentPrediction = predictionEngine.Predict(currentImageToPredict);
-
-                    Console.WriteLine(
-                        $"Image Filename : [{currentImageToPredict.ImageFileName}], " +
-                        $"Predicted Label : [{currentPrediction.PredictedLabel}], " +
-                        $"Probability : [{currentPrediction.Score.Max()}]");
-                }
+                var prediction = GetFirstPrediction(predictionEngine, imageToPredict);
+                DoSecondPrediction(predictionEngine, imageToPredict);
+                DoubleCheckUsingIndex(predictionEngine, prediction);
+                PrintInformationAboutPrediction(imageToPredict, prediction);
+                PredictAllImages(predictionEngine, imagesToPredict);
             }
             catch (Exception ex)
             {
@@ -94,6 +41,88 @@ namespace ImageClassification.Predict
 
             Console.WriteLine("Press any key to end the app..");
             Console.ReadKey();
+        }
+
+        private static void PredictAllImages(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, IEnumerable<InMemoryImageData> imagesToPredict)
+        {
+            //Predict all images in the folder
+            //
+            Console.WriteLine("");
+            Console.WriteLine("Predicting several images...");
+
+            foreach (var currentImageToPredict in imagesToPredict)
+            {
+                var currentPrediction = predictionEngine.Predict(currentImageToPredict);
+                PrintInformationAboutPrediction(currentImageToPredict, currentPrediction);
+            }
+        }
+
+        private static void PrintInformationAboutPrediction(InMemoryImageData imageToPredict, ImagePrediction prediction)
+        {
+            Console.WriteLine($"Image Filename : [{imageToPredict.ImageFileName}], " +
+                                              $"Predicted Label : [{prediction.PredictedLabel}], " +
+                                              $"Probability : [{prediction.Score.Max()}] "
+                                              );
+        }
+
+        private static void DoubleCheckUsingIndex(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, ImagePrediction prediction)
+        {
+            ////////
+            // Double-check using the index
+            var maxIndex = prediction.Score.ToList().IndexOf(prediction.Score.Max());
+            VBuffer<ReadOnlyMemory<char>> keys = default;
+            predictionEngine.OutputSchema[3].GetKeyValues(ref keys);
+            var keysArray = keys.DenseValues().ToArray();
+            var predictedLabelString = keysArray[maxIndex];
+            ////////
+        }
+
+        private static void DoSecondPrediction(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, InMemoryImageData imageToPredict)
+        {
+            // Measure #2 prediction execution time.
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+
+            var prediction2 = predictionEngine.Predict(imageToPredict);
+
+            // Stop measuring time.
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+            Console.WriteLine("Second Prediction took: " + elapsedMs2 + "mlSecs");
+        }
+
+        private static ImagePrediction GetFirstPrediction(PredictionEngine<InMemoryImageData, ImagePrediction> predictionEngine, InMemoryImageData imageToPredict)
+        {
+            // Measure #1 prediction execution time.
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var prediction = predictionEngine.Predict(imageToPredict);
+
+            // Stop measuring time.
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine("First Prediction took: " + elapsedMs + "mlSecs");
+            return prediction;
+        }
+
+        private static IEnumerable<InMemoryImageData> GetImagesToPredict(string imagesFolderPathForPredictions)
+        {
+
+            //Predict the first image in the folder
+            return FileUtils.LoadInMemoryImagesFromDirectory(imagesFolderPathForPredictions, false);
+        }
+
+        private static PredictionEngine<InMemoryImageData, ImagePrediction> GetPredictionEngine(MLContext mlContext, ITransformer loadedModel)
+        {
+
+            // Create prediction engine to try a single prediction (input = ImageData, output = ImagePrediction)
+            return mlContext.Model.CreatePredictionEngine<InMemoryImageData, ImagePrediction>(loadedModel);
+        }
+
+        private static ITransformer GetLoadedModel(string imageClassifierModelZipFilePath, MLContext mlContext)
+        {
+
+            // Load the model
+            return mlContext.Model.Load(imageClassifierModelZipFilePath, out var modelInputSchema);
         }
 
         public static string GetAbsolutePath(string relativePath)
